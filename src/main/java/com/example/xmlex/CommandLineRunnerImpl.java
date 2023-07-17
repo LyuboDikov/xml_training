efa89853435a8059ce7adb6487385b63650a83e7
LyuboDikov <lyubo.dikov@icloud.com>
2023-07-17 16:08:41 +0300

    Added files, created Model mapper bean, and CommandLineRunner impl

---
+++ src/main/java/com/example/xmlex/CommandLineRunnerImpl.java
@@ -0,0 +1,16 @@
+package com.example.xmlex;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/example/xmlex/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.xmlex.repositories;
+
+import com.example.xmlex.models.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
