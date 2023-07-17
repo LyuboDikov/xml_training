56760202d3c3da7f2220601fb5b58e4c9f1c5e1d
LyuboDikov <lyubo.dikov@icloud.com>
2023-07-17 16:35:52 +0300

    some changes

--- src/main/java/com/example/xmlex/CommandLineRunnerImpl.java
+++ src/main/java/com/example/xmlex/CommandLineRunnerImpl.java
@@ -1,9 +1,13 @@
 package com.example.xmlex;
 
+import com.example.xmlex.models.dtos.CategorySeedRootDto;
 import com.example.xmlex.services.CategoryService;
 import com.example.xmlex.util.XmlParser;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
@@ -25,6 +29,13 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
         seedData();
     }
 
-    private void seedData() {
+    private void seedData() throws JAXBException, FileNotFoundException {
+
+        CategorySeedRootDto categorySeedRootDto = xmlParser.fromFile(
+                FILES_PATH + CATEGORIES_FILE_NAME,
+                CategorySeedRootDto.class
+        );
+
+
     }
 }
--- src/main/java/com/example/xmlex/models/dtos/CategorySeedDto.java
+++ src/main/java/com/example/xmlex/models/dtos/CategorySeedDto.java
@@ -2,12 +2,14 @@ package com.example.xmlex.models.dtos;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "category")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class CategorySeedDto {
 
+    @XmlElement(name = "name")
     private String name;
 
     public String getName() {
